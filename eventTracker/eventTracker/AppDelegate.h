349fa2518408d2c1801c12e58184925d56de2c10
vishnunair <vishnu.g@armiasystems.com>
2014-12-20 22:19:59 +0530

    Initial Commit

---
+++ eventTracker/eventTracker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  eventTracker
+//
+//  Created by Developer on 12/20/14.
+//  Copyright (c) 2014 keepWorks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ eventTracker/eventTracker/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  eventTracker
+//
+//  Created by Developer on 12/20/14.
+//  Copyright (c) 2014 keepWorks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
